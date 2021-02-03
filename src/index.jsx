2f380290e552cffdb7800ead40b8c6352918f427
Mathieu JOLY <m.joly@live.fr>
2021-02-03 23:06:15 +0100

    add react

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>D o N o t e R e a c t</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
