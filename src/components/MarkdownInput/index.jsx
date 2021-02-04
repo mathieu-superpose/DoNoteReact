c3ee32678b822e1e9d1f89d607c447a99be6c16b
MarieDechaux <marie.dechaux@gmail.com>
2021-02-04 11:58:24 +0100

    fix displaynote

--- src/components/MarkdownInput/index.jsx
+++ src/components/MarkdownInput/index.jsx
@@ -1,22 +1,43 @@
+/*eslint-disable */
 import React from 'react';
 import './index.scss';
 
 // eslint-disable-next-line react/prop-types
 const MarkdownInput = ({ onInputValueChanged }) => {
-  const handleChange = (event) => {
-    const { value } = event.target;
+  const [title, setTitle] = React.useState('');
+  const [note, setNote] = React.useState('');
+
+  const handleOutputChange = () => {
+    const { value } = note;
+    console.log(note);
     onInputValueChanged(value);
+  };
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target);
+    handleOutputChange();
+  };
+
+  const handleNoteChange = (event) => {
+    setNote(event.target);
+    console.log(note);
+    handleOutputChange();
   };
 
   return (
     <form className="markdown-input">
-      <input type="text" className="title" maxLength="30" />
+      <input
+        type="text"
+        className="title"
+        maxLength="30"
+        onChange={handleTitleChange}
+      />
       <textarea
         id="story"
         name="story"
         rows="5"
         cols="33"
-        onChange={handleChange}
+        onChange={handleNoteChange}
       />
       <input type="submit" value="Envoyer" />
     </form>
